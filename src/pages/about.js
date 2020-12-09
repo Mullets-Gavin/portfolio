498fc00b8a67419f6db6b41681f11e18b512300b
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-09 17:51:05 -0500

    Moving stuff around

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,9 +3,9 @@ import Header from "../components/header"
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <Header headerText="About Gatsby" />
-      <Header headerText="It's pretty cool" />
+    <div style={{color: 'teal'}}>
+      <Header headerText="About Gatsby"/>
+      <Header headerText="It's pretty cool"/>
       <p>Such wow. Very React.</p>
     </div>
   )
