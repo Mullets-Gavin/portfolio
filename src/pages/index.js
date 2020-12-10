79246bb74f130cb76259ef710d2658a9488c865d
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-09 20:36:52 -0500

    Site design layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,45 @@
 import React from "react"
-import {Link} from "gatsby"
+import "../stylesheets/index.css"
+import "../stylesheets/styles.css"
+//import Button from "../components/buttons"
+//import { Link } from "gatsby"
 
-export default function Home() {
+const {useState} = React
+
+export default () => {
+  const [counter, setCounter] = useState(0);
+
   return (
-    <div style={{ color: `purple` }}>
-      <Link to="/contact/">Contact</Link>
-      <h1>Hello Gatsby!</h1>
-      <p>What a world.</p>
-      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <div className="Counter">
+      <div style={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '100%',
+          height: '100%'
+        }}>
+        
+        <h1>I am centered</h1>
+
+        <p>
+          {counter}
+        </p>
+
+        <button class="sexy-button" onClick={() => {
+            setCounter(counter + 1)
+        }}>Click me!</button>
+      </div>
     </div>
-  )
+  );
 }
+
+// export default function Home() {
+//   return (
+//     <div style={{ color: `purple` }}>
+//       <Link to="/contact/">Contact</Link>
+//       <h1>Hello Gatsby!</h1>
+//       <p>What a world.</p>
+//       <img src="https://source.unsplash.com/random/400x200" alt="" />
+//     </div>
+//   )
+// }
