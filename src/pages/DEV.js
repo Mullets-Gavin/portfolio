da36fda506ec4411e035ffe81785e5ec03d105f9
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-11 12:05:23 -0500

    Pushed dev page changes

---
+++ src/pages/DEV.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/styles.css'
+
+export default () => {
+    return (
+        <div class="panel"  className="Panel">
+
+
+            <div class="footer">this is my BANGER footer</div>
+        </div>
+    )
+}
