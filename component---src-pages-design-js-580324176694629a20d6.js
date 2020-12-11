54fbedc54cefd18239a125107b0ac842c0a4e5a6
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-11 12:07:09 -0500

    Updates

---
+++ component---src-pages-design-js-580324176694629a20d6.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{QRzb:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),a=t.n(u);e.default=function(){return a.a.createElement("h1",null,"hey")}}}]);
+//# sourceMappingURL=component---src-pages-design-js-580324176694629a20d6.js.map
