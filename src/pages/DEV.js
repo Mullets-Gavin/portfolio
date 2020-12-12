b64f7f68a43b46cbab3b86552e4a4de4c3ff57ef
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-12 00:51:57 -0500

    Much needed face lift

--- src/pages/DEV.js
+++ src/pages/DEV.js
@@ -13,15 +13,17 @@ export default () => {
                     <h1 class="header-title">Mullets.xyz</h1>
 
                     <div>
-                        <button class="header-button">this is text</button>
-                        <button class="header-button">this is text</button>
+                        <button class="header-button">Home</button>
+                        <button class="header-button">Games</button>
+                        <button class="header-button">Software</button>
+                        <button class="header-button">Contact</button>
                     </div>
                 </div>
 
                 <div class="info">
                     <div class="description">
                         <h2 class="description-title">Programmer and Game Developer</h2>
-                        <p class="description-content">I do be programming and making games doe</p>
+                        <p class="description-content">Creating exciting experiences since 2013</p>
 
                         <div class="avatar">
                             <img class="avatar-img" alt="Profile Pic" src={ProfilePic}></img>
@@ -30,7 +32,36 @@ export default () => {
                 </div>
 
                 <div class="about">
-                    <h1>ah fuck</h1>
+                    <div class="about-content">
+                        <h1 class="about-title">Hi, my name is Gavin Rosenthal</h1>
+
+                        <p class="about-body">
+                            I've been an aspiring programmer and game developer ever since I was introduced to the game platform Roblox in 2013 at the
+                            age of twelve. Previously, I’ve volunteered at Roblox as a member of their Post Approval team in their forums, which aims
+                            to help users who are starting their Roblox careers to receive the help and knowledge needed to create insightful posts and
+                            to learn new skills. I’m also a former intern at the company as an accelerator, where I worked on creating an immersive
+                            experience in Roblox alongside some of the best people in the industry, such as professional game designers, product managers,
+                            and engineers.
+                        </p>
+                        
+                        <br></br>
+
+                        <p class="about-body">
+                            I’m completely self-taught, and I aspire to perfect my craft by practicing & learning new languages and software, such as React.
+                            My interests include puzzle solving, creating open source software, and designing & creating websites. When I’m not programming,
+                            I like to go for long walks, watch movies with friends, and play Apex Legends.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="skills">
+                    <h1 class="skills-title">Skills</h1>
+
+                    <div class="skills-content">
+                        <div class="skills-column">
+                            wtf
+                        </div>
+                    </div>
                 </div>
 
                 <div class="footer">
