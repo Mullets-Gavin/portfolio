0a572162178bc351e07e3b79daa4f43ef17727c9
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-15 10:05:18 -0500

    Footer finalized

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,8 +3,21 @@ import React from 'react'
 export default () => {
     return (
         <div class="footer">
-            <div class="footer-content">
-                <h2 class="footer-title">Directory</h2>
+            <div class="footer-directory">
+                <div class="footer-content">
+                    <div class="footer-buttons">
+                        <a class="footer-map" href="" rel="noreferrer" role="button">Home</a>
+                        <a class="footer-map" href="games" rel="noreferrer" role="button">Games</a>
+                        <a class="footer-map" href="software" rel="noreferrer" role="button">Software</a>
+                        <a class="footer-map" href="contact" rel="noreferrer" role="button">Contact</a>
+                    </div>
+
+                    <hr class="footer-credit"/>
+
+                    <div class="footer-credit">
+                        <p>Made by ♥ by Gavin Rosenthal | <a class="footer-map" href="https://github.com/Mullets-Gavin/Mullets-Gavin.github.io" rel="noreferrer" role="button">Source on Github</a></p>
+                    </div>
+                </div>
             </div>
         </div>
     )
