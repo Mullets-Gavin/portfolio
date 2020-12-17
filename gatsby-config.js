00872ace9e725c999dae35ba4e54576cf48c66ec
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-17 06:37:35 -0500

    Cleaning files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,6 @@
-/*
-  See: https://www.gatsbyjs.com/docs/gatsby-config/
-*/
-
 module.exports = {
   pathPrefix: "/Mullets-Gavin.github.io",
-  plugins: ['gatsby-plugin-react-helmet'],
   siteMetadata: {
     title: "Gavin"
-  }
+  },
 }
