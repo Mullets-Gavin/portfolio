3306d7ec6d7bac4b15b7e4a8dae3736920b22f24
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-25 01:13:16 -0500

    Attempting metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  pathPrefix: "/Mullets.xyz",
-  siteMetadata: {
-    title: "Gavin"
-  },
-}
+	pathPrefix: '/Mullets.xyz',
+	siteMetadata: {
+		title: 'Mullets',
+		titleTemplate: '%s | Programmer & Game Designer',
+		description: 'A straight up nerd who enjoys Lua and JavaScript',
+		url: 'https://mullets.xyz', // No trailing slash allowed!
+		image: '/static/mafiadev.png', // Path to your image you placed in the 'static' folder
+		twitterUsername: '@mullets_gavin',
+	},
+};
