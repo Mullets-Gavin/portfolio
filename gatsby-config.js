f94d416b0093a0a821b8e8f710a56d8bd8e290af
Gavin Rosenthal <38087728+Mullets-Gavin@users.noreply.github.com>
2020-12-25 12:08:46 -0500

    Added SEO

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,7 @@ module.exports = {
 		titleTemplate: '%s | Programmer & Game Designer',
 		description: 'A straight up nerd who enjoys Lua and JavaScript',
 		url: 'https://mullets.xyz', // No trailing slash allowed!
-		image: '/static/mafiadev.png', // Path to your image you placed in the 'static' folder
+		image: '/images/logo.png', // Path to your image you placed in the 'static' folder
 		twitterUsername: '@mullets_gavin',
 	},
 };
