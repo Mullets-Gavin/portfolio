be048c755202c2672d937490054dc0bf97b03cc0
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2021-10-08 13:37:37 -0700

    site additions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 import Accent from "../components/accent";
+import Career from "../components/career";
 import Games from "../components/games";
 import Header from "../components/header";
+import Title from "../components/title";
 import "../stylesheets/styles.css";
 
 export default () => {
@@ -16,8 +18,14 @@ export default () => {
 			<div className="document">
 				<div className="space" />
 				<Header />
+				{/* <div className="space" />
+				<Title title="My Games" /> */}
 				<div className="space" />
 				<Games />
+				{/* <div className="space" />
+				<Title title="My Career" /> */}
+				<div className="space" />
+				<Career />
 				<div className="space" />
 			</div>
 		</div>
