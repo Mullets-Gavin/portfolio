5bcfdfad94e1d313d73e903b5c5dcd2ac09e8588
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2024-02-11 01:26:50 -0800

    big numbers

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -6,25 +6,26 @@ interface StatsProps {}
 export default class Stats extends Component<StatsState, StatsProps> {
 	render() {
 		return (
-			<div className="stats-grid">
-				<div className="card corner pillow stats">
-					<h1>84.3m+</h1>
-					<h3>Total Plays</h3>
+			<div>
+				<div className="card corner stats-title">
+					<div className="pillow">
+						<h1>84.3m+</h1>
+						<h3>Total Plays</h3>
+					</div>
 				</div>
 
-				<div className="card corner pillow stats">
-					<h1>657k+</h1>
-					<h3>Total Favorites</h3>
-				</div>
+				<div className="space" />
 
-				<div className="card corner pillow stats">
-					<h1>663k+</h1>
-					<h3>Total Community</h3>
-				</div>
+				<div className="stats-grid">
+					<div className="card corner pillow stats">
+						<h1>657k+</h1>
+						<h3>Total Favorites</h3>
+					</div>
 
-				<div className="card corner pillow stats">
-					<h1>104k+</h1>
-					<h3>Total Likes</h3>
+					<div className="card corner pillow stats">
+						<h1>663k+</h1>
+						<h3>Total Members</h3>
+					</div>
 				</div>
 			</div>
 		);
