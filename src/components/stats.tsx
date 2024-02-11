a59bede40c472d7438c310f63dc34bd44d5a7663
Mullets-Gavin <38087728+Mullets-Gavin@users.noreply.github.com>
2024-02-10 21:29:32 -0800

    changed numeros

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -8,12 +8,12 @@ export default class Stats extends Component<StatsState, StatsProps> {
 		return(
 		<div className="stats-grid">
 			<div className="card corner pillow stats stats-right">
-				<h1>84,380,000+</h1>
+				<h1>84.3m+</h1>
 				<h3>Total Plays</h3>
 			</div>
 
 			<div className="card corner pillow stats stats-left">
-				<h1>657,000+</h1>
+				<h1>657k+</h1>
 				<h3>Total Favorites</h3>
 			</div>
 		</div>
